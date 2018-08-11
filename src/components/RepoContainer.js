51a85f10a15cf830beccd313016d25f6fadcd606
Akash <akash.samantra19@gmail.com>
2018-08-11 21:26:42 +0530

    Repo component working fine

---
+++ src/components/RepoContainer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const RepoContainer = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardContent className="card">
+          <h3>{props.name}</h3>
+          <p style={{ fontSize: '13px' }}>{props.desc}</p>
+          <div className="flex-container">
+            <p>{props.language}</p>
+            <p>
+              <img className="small-image" src={require('../assets/star-24px.svg')} />
+              {props.starCount}
+            </p>
+            <p>{props.forkCount}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+RepoContainer.propTypes = {
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  starCount: PropTypes.number.isRequired,
+  forkCount: PropTypes.number.isRequired
+}
+
+export default RepoContainer;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import RepoContainer from './RepoContainer';
 
 class Repos extends Component {
-  render(props) {
+  state = {
+    filterKeyword: ''
+  }
+
+  changefilterKeywordHandler = (event) => {
+    this.setState({ filterKeyword: event.target.value })
+  }
+
+  render() {
+    const repos = this.props.data;
     return (
       <div>
-        Repos
+        <div className="filter">
+          <input
+            className="filterTextField"
+            type="text"
+            placeholder="Enter repo to filter"
+            value={this.state.username}
+            onChange={this.changefilterKeywordHandler}
+          />
+        </div>
+        <div className="repo-container">
+          <Grid container spacing={24}>
+            {
+              repos.map(repo => {
+                return (
+                  <Grid item xs={12} sm={6} key={repo.id}>
+                    <RepoContainer
+                      name={repo.name}
+                      desc={repo.description}
+                      language={repo.language}
+                      starCount={repo.stargazers_count}
+                      forkCount={repo.forks_count}
+                    />
+                  </Grid>
+                );
+              })
+            }
+          </Grid>
+        </div>
       </div>
     );
   }
